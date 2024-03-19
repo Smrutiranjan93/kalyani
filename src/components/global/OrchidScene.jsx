ca4d6da15841e71a3518f5f1085b24f9891d3588
Deepak Kumar Gouda <deepakkumargouda425@gmail.com>
2024-03-19 11:43:36 +0530

    all hotspot removed

--- src/components/global/OrchidScene.jsx
+++ src/components/global/OrchidScene.jsx
@@ -45,7 +45,7 @@ export default function ParijatScene() {
         autoLoad={true}
         showZoomCtrl={false}
         showFullscreenCtrl={false}
-        hotspotDebug={true}
+        hotspotDebug={false}
       >
         {Object.values(scene.hotSpots).map((Element, i) =>
           hotSpots(Element, i)
